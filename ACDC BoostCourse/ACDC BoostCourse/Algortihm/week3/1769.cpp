2dce9317bef12aab9a02f1841b1c12cd0ea6d781
Yeongjin <njun136@naver.com>
2022-07-26 20:51:55 +0900

    [장영진] 4주차 과제

--- ACDC BoostCourse/ACDC BoostCourse/Algortihm/week3/1769.cpp
+++ ACDC BoostCourse/ACDC BoostCourse/Algortihm/week3/1769.cpp
@@ -12,13 +12,11 @@ int main() {
 	while(num.length() != 1) {
 		for (int i = 0; i < num.length(); i++) {
 			sum += num[i];
-			cout << sum << endl;
 		}
-
 		num = sum;
 		cnt++;
 	}//num의 길이가 1이 아닐 동안 반복되어야 하는데 한 번만 도는 이유를 모르겠다.
-	//합의 값이 이상한 것을 보아 sum=num 구문에서 문제가 발생하는 것 같다.
+	//합의 값이 이상한 것을 보아 sum+=num[i] 구문에서 문제가 발생하는 것 같다.
 	if (sum == 3 || sum == 6 || sum == 9) {
 		cout << cnt << endl;
 		cout << "YES" << endl;
